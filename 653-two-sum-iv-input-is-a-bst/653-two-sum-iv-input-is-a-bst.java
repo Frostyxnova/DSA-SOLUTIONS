20bc0c44e0b03cc2ce9ddf9ade15c74d39c86dc2
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2022-10-10 00:18:11 +0530

    Time: 5 ms (81.36%), Space: 52 MB (26.66%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -16,11 +16,13 @@
 class Solution {
     Set<Integer> set = new HashSet<>();
     public boolean findTarget(TreeNode root, int k) {
-    if(root == null) return false;
-        if(set.contains(k - root.val)) return true;
-        else{
-            set.add(root.val);
+    if(root == null) return false;   
+        if(set.contains(k-root.val)){
+            return true;
         }
-        return findTarget(root.right,k) || findTarget(root.left,k);
+        else{
+               set.add(root.val);
+        }
+        return findTarget(root.left,k) || findTarget(root.right,k);
     }
 }

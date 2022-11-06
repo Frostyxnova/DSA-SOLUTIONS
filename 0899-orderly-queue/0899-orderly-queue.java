bd433a35fe0c1661430d6166bd068473ffe14582
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2022-11-06 14:12:44 +0530

    Time: 8 ms (49.50%), Space: 45.3 MB (39.60%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String orderlyQueue(String s, int k) {
+    if(k == 1){
+        String ans = s;
+        for(int i = 0; i < s.length(); i++){
+           String temp = s.substring(i) + s.substring(0,i);
+            if(temp.compareTo(ans) < 0) ans = temp;
+        }
+        return ans;
+    }
+        else{
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            return new String(arr);
+        }
+    }
+}

aaab4ca0439aea0a455cffda0d9cfaeafe4b81f7
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2022-11-07 11:58:32 +0530

    Time: 6 ms (27.76%), Space: 41.2 MB (32.93%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,15 +1,5 @@
 class Solution {
     public int maximum69Number (int num) {
-      String nums= String.valueOf(num);
-        char[] chars = nums.toCharArray();
-
-        for(int i=0; i<chars.length; i++){
-            if(chars[i]=='6'){
-                chars[i]='9';
-                break;
-            }
-        }
-
-        return Integer.valueOf(String.valueOf(chars));    
+    return Integer.parseInt(String.valueOf(num).replaceFirst("6","9"));    
     }
 }

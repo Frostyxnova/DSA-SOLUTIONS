ae5dd6929750f38a6eed060eee2516ed99cfc76b
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2022-11-07 12:33:38 +0530

    Added solution - LeetHub

---
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j]  =Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortest_distance(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j  = 0; j < n; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+    int n = matrix.length;
+    for(int m = 0; m < n; m++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][m] == -1 || matrix[m][j] == -1) continue;
+                if(matrix[i][j] == -1 || matrix[i][m] + matrix[m][j] < matrix[i][j]) matrix[i][j] = matrix[i][m] + matrix[m][j];
+                
+            }
+        }
+    }
+    }
+}

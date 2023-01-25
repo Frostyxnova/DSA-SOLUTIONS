5229f7c0d7beaa7fee3af9a052b9bbba643ea320
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-01-26 00:01:06 +0530

    Added solution - LeetHub

--- Type it! - GFG/type-it.java
+++ Type it! - GFG/type-it.java
@@ -28,9 +28,15 @@ class GFG {
 class Solution {
     int minOperation(String s) {
        int ans=s.length();
-       // char[] c=s.toCharArray();
-        for(int i=0;i<s.length()/2;i++) {
-           if(s.substring(0,i+1).equals(s.substring(i+1,i+i+2)))ans = s.length()-i;
+       char[] c=s.toCharArray();
+       for(int i=0;i<s.length()/2;i++) {
+            boolean isValid=true;
+            for(int j=0;j<=i && isValid;j++) {
+                if(c[j]!=c[j+i+1]) {
+                    isValid=false;
+                }
+            }
+            if(isValid) ans = s.length()-i;
         }
         return ans;
     }

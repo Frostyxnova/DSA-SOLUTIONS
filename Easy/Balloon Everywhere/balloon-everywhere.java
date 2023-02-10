9edc94e0f7713690033816a4d58bda09cb98c8d9
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-02-10 23:27:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Balloon Everywhere/balloon-everywhere.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.maxInstance(s);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int maxInstance(String s) {
+    int[] v = new int[26];
+    for(int i: s.toCharArray()){
+        v[i-'a']++;
+    }
+     return Math.min(Math.min(v[0],v[1]),Math.min(Math.min(v[11]/2,v[14]/2),v[13]));
+    }
+}

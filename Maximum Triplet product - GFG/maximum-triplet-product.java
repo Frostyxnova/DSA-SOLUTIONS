ce176cacbe2d555a88e7c34ca8f9e462a04154d6
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-03-10 12:17:50 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.java
+++ Maximum Triplet product - GFG/maximum-triplet-product.java
@@ -13,9 +13,30 @@ import java.util.*;
 class Solution {
     Long maxTripletProduct(Long arr[], int n)
     {
-    Arrays.sort(arr);
-
-    return Math.max(arr[n-1]*arr[n-2]*arr[n-3], arr[0]*arr[1]*arr[n-1]);
+     long mx1=Integer.MIN_VALUE;
+        long mx2=mx1,mx3=mx1;
+        long mn1=Integer.MAX_VALUE;
+        long mn2=mn1;
+        for(long val : arr) {
+            if(mx1<=val) {
+                mx3=mx2;
+                mx2=mx1;
+                mx1=val;
+            } else if(mx2<=val) {
+                mx3=mx2;
+                mx2=val;
+            } else if(mx3<=val) {
+                mx3=val;
+            }
+            
+            if(mn1>=val) {
+                mn2=mn1;
+                mn1=val;
+            } else if(mn2>=val) {
+                mn2=val;
+            }
+        }
+        return Math.max(mn1*mn2*mx1,mx1*mx2*mx3);
     }
 }
 

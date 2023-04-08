f3477e83bb4cdae2dbee0265448c7b82637713a4
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-04-08 21:00:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Make the array beautiful/make-the-array-beautiful.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.makeBeautiful(arr);
+            for (int e : res) out.print(e + " ");
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> makeBeautiful(int[] arr) {
+    ArrayList<Integer> ans = new ArrayList<>();
+    for(int i : arr){
+        if(ans.size() == 0){
+            ans.add(i);
+        }
+        else if((ans.get(ans.size()-1) >= 0 && i < 0) || (ans.get(ans.size()-1) < 0 && i >= 0)){
+            ans.remove(ans.size()-1); // pop operation
+        }
+        else{
+            ans.add(i); // push operation
+        }
+    }
+    return ans;
+    }
+}

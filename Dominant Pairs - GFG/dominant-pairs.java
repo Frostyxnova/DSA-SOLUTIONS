81385abf88ee0bf9072c4ad6804b5b5160af5c93
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-04-12 11:39:39 +0530

    Added solution - LeetHub

---
+++ Dominant Pairs - GFG/dominant-pairs.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.dominantPairs(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int dominantPairs(int n, int[] arr) {
+    Arrays.sort(arr,n/2,n);
+    Arrays.sort(arr,0,n/2);
+    int cnt = 0, finalCount = 0;
+    int j = n/2;
+    boolean flag = false;
+    for(int i = 0; i < n/2; i++){
+        while(j < n && arr[i] >= (5*arr[j])){
+            cnt++;
+            j++;
+            flag = true;
+        }
+        if(flag){
+            finalCount+= cnt;
+        }
+    }
+    return finalCount;
+    }
+}
+        

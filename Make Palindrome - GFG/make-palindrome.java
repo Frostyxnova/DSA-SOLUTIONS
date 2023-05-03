05ddad983749d1bd6f5819a200e59172172da7d3
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-05-03 22:58:09 +0530

    Added solution - LeetHub

---
+++ Make Palindrome - GFG/make-palindrome.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class StringArray
+{
+    public static String[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        return s;
+    }
+
+    public static void print(String[] a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<String> a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            String[] arr = StringArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.makePalindrome(n, arr);
+            
+            String _result_val = (res) ? "YES" : "NO";
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+   public static boolean palindrome(String str)
+    {
+        int low=0,high=str.length()-1;
+        
+        while(low<high)
+        {
+            if(str.charAt(low++)!=str.charAt(high--))
+            return false;
+        }
+        
+        return true;
+    }
+    public static boolean makePalindrome(int n, String[] arr) {
+        Map<String, Integer> map = new HashMap<>();
+        Map<String,Integer> palins=new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            String str = arr[i];
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+    
+        int cnt=0;
+        for (int i = 0; i < n; i++) {
+            if(!palindrome(arr[i])){
+                String str = arr[i];
+                String rev = new StringBuffer(str).reverse().toString();
+        
+                if (!map.get(str).equals(map.get(rev))) {
+                        return false;
+                }
+            }
+            else
+            {
+                palins.put(arr[i],palins.getOrDefault(arr[i],0)+1);
+            }
+        
+        }
+        
+        for(var freq:palins.values())
+        {
+            if(freq%2==1) cnt++; 
+        }
+        
+        
+    
+        return cnt<=1;
+    }
+}

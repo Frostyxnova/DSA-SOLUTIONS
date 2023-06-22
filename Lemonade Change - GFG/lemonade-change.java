b8f63ecf0db152c843b6c91a34dd8c4c277aebef
Farhan Khan <90808223+Frostyxnova@users.noreply.github.com>
2023-06-22 15:54:18 +0530

    Added solution - LeetHub

---
+++ Lemonade Change - GFG/lemonade-change.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            boolean ans = obj.lemonadeChange(n, a);
+            System.out.println(ans ? "True" : "False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static boolean lemonadeChange(int N, int bills[]) {
+    int count5 = 0;
+    int count10 = 0;
+    for(int i = 0; i < N; i++){
+        if(bills[i] == 0) continue;
+        if(bills[i] == 5) count5++;
+        if(bills[i] == 10){
+            if(count5 == 0){
+                return false;
+            }
+            count5--;
+            count10++;
+        }
+        if(bills[i] == 20){
+            if(count10 > 0 && count5 > 0){
+                count5--;
+                count10--;
+            }
+            else if(count5 >= 3){
+                count5 -= 3;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+    }
+}
